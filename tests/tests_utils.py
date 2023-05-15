362895312082fa65bd6c7240b980a81809f843ef
Your Name <you@example.com>
2023-05-15 16:36:07 +0800

    :)

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -15,4 +15,34 @@ class Testutils(TestCase):
         sum = self.util.add(1, 2)
 
         self.assertEqual(sum, 3)
+
+
+
+    def test_subtract_raises_ValueError_when_given_argument_is_not_Integer(self):
+        with self.assertRaises(ValueError):
+            self.util.subtract(1, 'test')
+
+    def test_subtract_returns_difference_of_input_arguments(self):
+        difference = self.util.subtract(3, 2)
+        self.assertEqual(difference, 1)
+
+    def test_multiply_raises_ValueError_when_given_argument_is_not_Integer(self):
+        with self.assertRaises(ValueError):
+            self.util.multiply(2, 'test')
+
+    def test_multiply_returns_product_of_input_arguments(self):
+        product = self.util.multiply(3, 2)
+        self.assertEqual(product, 6)
+
+    def test_divide_raises_ValueError_when_given_argument_is_not_Integer(self):
+        with self.assertRaises(ValueError):
+            self.util.divide(1, 'test')
+
+    def test_divide_raises_ValueError_when_divisor_is_zero(self):
+        with self.assertRaises(ValueError):
+            self.util.divide(1, 0)
+
+    def test_divide_returns_quotient_of_input_arguments(self):
+        quotient = self.util.divide(6, 2)
+        self.assertEqual(quotient, 3)
         
--- utils/utils.py
+++ utils/utils.py
@@ -7,4 +7,25 @@ class Utils():
         if not isinstance(a, int) or not isinstance(b, int):
             raise ValueError('The input argument should be Integer')
         
-        return a+b
+        return a + b
+
+    def subtract(self, a, b):
+        if not isinstance(a, int) or not isinstance(b, int):
+            raise ValueError('The input argument should be Integer')
+
+        return a - b
+
+    def multiply(self, a, b):
+        if not isinstance(a, int) or not isinstance(b, int):
+            raise ValueError('The input argument should be Integer')
+
+        return a * b
+
+    def divide(self, a, b):
+        if not isinstance(a, int) or not isinstance(b, int):
+            raise ValueError('The input argument should be Integer')
+
+        if b == 0:
+            raise ValueError('Cannot divide by zero')
+
+        return a / b
